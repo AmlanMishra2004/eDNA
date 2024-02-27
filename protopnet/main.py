5dfdcc06f8d1fbf9619f0a5f0d898f46cf645947
Sam Waggoner <samwags3@gmail.com>
2024-02-27 12:50:56 -0500

    increased gap push

--- protopnet/main.py
+++ protopnet/main.py
@@ -318,7 +318,7 @@ for trial in range(1):
         }], 
         'last_layer_optimizer_lr':  [0.001], #random.uniform(0.0001, 0.001) # jon: 0.02, sam's OG: 0.002
         'num_warm_epochs':          [1_000_000], # random.randint(0, 10) # not set
-        'push_epochs_gap':          [6], # 17 #random.randint(10, 20)# 1_000_000 # not set
+        'push_epochs_gap':          [11], # 17 #random.randint(10, 20)# 1_000_000 # not set
         'push_start':               [10], #25 #random.randint(20, 30) # 1_000_000 #random.randint(0, 10) # not set #10_000_000
         # BELOW IS UNUSED
         'joint_lr_step_size':       [-1], #random.randint(1, 20) # not set, 20 is arbitrary and may or may not be greater than the number of epochs
