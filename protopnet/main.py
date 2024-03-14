f087c9b459f132901240e3b2e28b83a90abf0fd0
Sam Waggoner <samwags3@gmail.com>
2024-03-14 17:38:47 -0400

    -

--- protopnet/main.py
+++ protopnet/main.py
@@ -170,7 +170,7 @@ X_train, X_val, y_train, y_val = train_test_split(
 # print(type(X_train))
 # print(type(y_train))
 
-train = np.column_stack((X_train, y_train)) # USED TO BE train = pd.concat([X_train, y_train], axis=1)
+train = X_train.join(y_train) # USED TO BE train = pd.concat([X_train, y_train], axis=1)
 
 # print("success")
 # wait=input("pause")
