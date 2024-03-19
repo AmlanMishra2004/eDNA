94602c0d06e6cad26f5a439ba58c6216de911a2c
Samuel Waggoner (SCIS - Chen) <swaggoner@katahdin.acg.maine.edu>
2024-03-19 07:37:57 -0400

    added the result file so i don't have to import matplotlib

---
+++ graph_val_acc.py
@@ -0,0 +1,27 @@
+import matplotlib.pyplot as plt
+import re
+
+# Open the file
+with open('out.1831432.log', 'r') as file:
+    data = file.read()
+
+# Split the data into different combinations
+combinations = data.split('Attempting combination')
+
+# For each combination
+for i in range(1, len(combinations)):
+    # Extract the validation accuracies
+    val_accs = re.findall('Val acc before epoch \d+: (\d+\.\d+)', combinations[i])
+
+    # Convert to floats
+    val_accs = [float(acc) for acc in val_accs]
+
+    # Generate the graph
+    plt.figure(figsize=(10, 6))
+    plt.plot(val_accs)
+    plt.title(f'Validation Accuracy for Combination {i}')
+    plt.xlabel('Epoch')
+    plt.ylabel('Validation Accuracy')
+    plt.grid(True)
+    plt.show()
+
