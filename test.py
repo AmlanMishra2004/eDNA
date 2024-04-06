943182aed8e85828184f02cd60de6606acd6103a
Sam Waggoner <samwags3@gmail.com>
2024-04-06 00:23:15 -0400

    -

--- test.py
+++ test.py
@@ -1,9 +1,10 @@
 import sys
 
-def main(index):
-    # Your code here
-    print(f"Running job with index {index}")
+def main(job, index):
+    print("Running job with job {}".format(job))
+    print("Running job with index {}".format(index))
 
 if __name__ == "__main__":
-    index = int(sys.argv[1])
-    main(index)
+    index = int(sys.argv[2])
+    job = int(sys.argv[1])
+    main(job, index)
