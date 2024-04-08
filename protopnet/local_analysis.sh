a8293cba8c429ef4dff6fd3b3a8b039cc2c22922
Sam Waggoner <samwags3@gmail.com>
2024-04-07 23:56:52 -0400

    trying local analysis

---
+++ protopnet/local_analysis.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+#SBATCH --job-name=local_analysis  # Job name
+# not SBATCH --mail-type=ALL          # Mail events (NONE, BEGIN, END, FAIL, ALL)
+# not SBATCH --mail-user=rose.gurung@maine.edu     # Where to send mail
+#SBATCH --output=data_prep_%j.out
+#SBATCH --ntasks=1                    # Run on a single Node
+#SBATCH --cpus-per-task=24
+#SBATCH --mem=12gb                     # Job memory request
+#SBATCH --time=10:00:00               # Time limit hrs:min:sec
+#SBATCH --partition=dgx
+#SBATCH --gres=gpu:1
+
+    # srun -u python3 local_analysis.py -gpuid 0\
+    #                                 -modeldir './local_results'\
+    #                                 -model '250_19push0.7820.pth'\
+    #                                 -savedir ./test_local_seq_$IND\
+    #                                 -targetrow $IND \
+    #                                 -sequence 'ACGTCGTGTGTGTGTGTGTGGGT'\
+    #                                 -seqclass 1
+
+module load singularity
+
+source ../eDNA_env/bin/activate
+cd protopnet
+
+for IND in 25 50 75 100 125 150 175 200
+do
+    singularity run --nv ~/containers/pytorch-waggoner2.simg python3 local_analysis.py\
+                                    -gpuid 0\
+                                    -modeldir './saved_ppn_models'\
+                                    -model '1857326_0.9894.pth'\
+                                    -savedir ./local_results/test_local_seq_$IND\
+                                    -targetrow $IND \
+                                    -sequence 'ACGTCGTGTGTGTGTGTGTGGGT'\
+                                    -seqclass 1
+done
+
